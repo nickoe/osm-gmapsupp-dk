3070361b0063bf949833fe618cff2a2d7cfd6bad
Nick Østergaard <oe.nick@gmail.com>
2012-06-19 12:32:56 +0200

    Log format

--- makeall.sh
+++ makeall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo -ne "Updated: "
-date
-echo ""
+echo -ne "Updated: " >> /tmp/nickoe/osm-gmapsupp-dk/update.log
+date >> /tmp/nickoe/osm-gmapsupp-dk/update.log
+echo "" >> /tmp/nickoe/osm-gmapsupp-dk/update.log
 cd /tmp/nickoe/osm-gmapsupp-dk
 /usr/bin/make all
